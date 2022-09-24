b2ff76ea14429d55e67c542be89db093a0756344
BRIAN MASON <bmasonn@yahoo.com>
2022-09-23 22:02:58 -0400

    added more code

---
+++ src/main/java/com/promineotech/jeep/JeepSales.java
@@ -0,0 +1,21 @@
+package com.promineotech.jeep;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class JeepSales implements CommandLineRunner{
+
+  public static void main(String[] args) {
+    SpringApplication.run(JeepSales.class, args);
+    
+    }
+
+  @Override
+  public void run(String... args) throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
